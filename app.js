cac9c8ef2fb52086280d362cc19e920eb8a30653
leslyrevenge <leslyrevenge@gmail.com>
2018-03-26 14:14:40 -0400

    8. Lesson: This, Challenge: function expression, return

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-/**** Lesson: Function Expression */
-
-//Challenge: create an object, timeline, that have key value for the following: birth year, today year, high school grad year. 
+/**** Lesson: this */ 
 
 var timeline = {
     birthYear: 1980,
     todayYear: 2018,
     gradYear: 1998,
     gradAge: function(){
-        return 1998 - 1980;
+        return this.gradYear - 1980;
+    },
+    myAge: function(){
+        return this.todayYear - this.birthYear;
     }
 }
 
-console.log(timeline.gradAge());
+console.log(timeline.myAge());
+
+//Challenge: Create a function expression that ---return--- your favorite greeting.
+
+var greeting = function(){
+    return "My favorite color is brown";
+}
+
+console.log(greeting());
