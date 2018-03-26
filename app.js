2516cd465157329d0c9e237696e8fc91da1d8210
leslyrevenge <leslyrevenge@gmail.com>
2018-03-26 13:57:50 -0400

    Lesson: return, Challenge: function, subtraction

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-/**** Lesson: Subtraction */
+/**** Lesson: Return */
 
-function myAge(){
-    var yob = 1995;
-    var year = 2018;
-    var myAge = year - yob;
-    console.log(myAge);
+//Challenge: create a function that console.log todays year minus the year you graduated high school or about to graduate. 
+
+function yearSinceGrad(){
+    return 2018 - 2005;
 }
 
-myAge();
 
-//Challenge: create an array, inside a function, that console log your top three favorite colors. 
-
-function myColors() {
-    var favColors = ["Brown", "Blue", "Green"]
-    console.log(favColors[2]);
-}
-
-myColors();
+console.log(31 - yearSinceGrad())
