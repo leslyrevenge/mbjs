8133866a8badfcd6c3773ca7b686bc1da5b35ea7
leslyrevenge <leslyrevenge@gmail.com>
2018-03-27 13:23:45 -0400

    11. Lesson: Extends, Challenge: params

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-/**** Lesson: Classes */ 
+/**** Lesson: Extends (inherritance) */ 
 
 class Haiti{
     constructor(){
         this.capital = "Port-au-Prince";
         this.militia = "Ton Ton Macout";
         this.leader = "Pa Pa Doc";
+        this.iYear = 1804
     }
     getCapital(){
         return this.capital;
@@ -14,12 +15,15 @@ class Haiti{
     }
 }
 
+class Independence extends Haiti{
+    getIndependenceSince(){
+        return 2018 - this.iYear;
+    }
+}
 var haiti = new Haiti;
 console.log(haiti.getMilitia());
 
-//Challenge: create a function that subtract two numbers. One of the numbers must be a param. 
+var haitiIndependence = new Independence;
+console.log(haitiIndependence.getIndependenceSince());
 
-function subtract(year){
-    return year - 1980;
-}
-console.log(subtract(1998));
+//Challenge: use params intead of hard codinging year.
