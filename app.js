202adc04b5159894b299aeba649e21a53b42ba63
leslyrevenge <leslyrevenge@gmail.com>
2018-03-27 09:47:24 -0400

    10. Lesson: Classes, Challenge: parameters, function, subtraction

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-/**** Lesson: Params */ 
-function myName (myString) {
-    return myString;
+/**** Lesson: Classes */ 
+
+class Haiti{
+    constructor(){
+        this.capital = "Port-au-Prince";
+        this.militia = "Ton Ton Macout";
+        this.leader = "Pa Pa Doc";
+    }
+    getCapital(){
+        return this.capital;
+    }
+    getMilitia(){
+        return this.militia;
+    }
 }
 
-console.log(myName("my name is Lesly"));
-//Challenge: create a function expression that output your city of birth. Make the city of birth a param
+var haiti = new Haiti;
+console.log(haiti.getMilitia());
 
+//Challenge: create a function that subtract two numbers. One of the numbers must be a param. 
 
-function cityOfBirth (city){
-    return city;
+function subtract(year){
+    return year - 1980;
 }
-
-console.log(cityOfBirth("Port au Prince"));
+console.log(subtract(1998));
